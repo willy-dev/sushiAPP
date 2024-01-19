aa0f2e3c1b2bd292c08f79dc1b33179e6169a2ee
Wilfred Marete <marete015@gmail.com>
2024-01-19 15:59:33 +0300

    edited the menu page banner

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi/components/button.dart';
+import 'package:sushi/theme/colors.dart';
+import '';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -13,12 +17,85 @@ class _MenuPageState extends State<MenuPage> {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Icon(
           Icons.menu,
           color: Colors.grey[900],
         ),
+        title: Text(
+          'Tokyo',
+          style: TextStyle(color: Colors.grey[900]),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 25,
+              horizontal: 30,
+            ),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //promo banner
+                    Text(
+                      'Get 32% Promo',
+                      style: GoogleFonts.dmSerifDisplay(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+                    //redeem button
+                    MyButton(
+                      text: 'Redeem',
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+                //image
+                Image.asset(
+                  'lib/images/many_sushi.png',
+                  height: 100,
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 25),
+          //search bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          //menu list
+
+          //popular food
+        ],
       ),
     );
   }
